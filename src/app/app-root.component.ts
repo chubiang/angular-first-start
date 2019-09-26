6826509810b7c63b175d47c4bd5c7cf14cb18450
chubiang <jiyeeon1208@gmail.com>
2019-09-26 14:30:35 +0900

    1 file modified, 4 added, 3 deleted

--- src/app/app-root.component.ts
+++ src/app/app-root.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  templateUrl: './app-root.component.html',
+  styleUrls: [ './app-root.component.css' ]
 })
-export class AppComponent  {}
+export class AppRootComponent  {}
 
 
 /*
